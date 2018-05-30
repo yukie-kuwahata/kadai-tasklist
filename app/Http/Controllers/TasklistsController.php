b79ee578940a3638b2331ea087d4659c60841536
jiro.kirameki <ec2-user@ip-172-31-9-32.us-east-2.compute.internal>
2018-05-30 07:29:05 +0000

    validation

--- app/Http/Controllers/TasklistsController.php
+++ app/Http/Controllers/TasklistsController.php
@@ -45,7 +45,12 @@ class TasklistsController extends Controller
      * @return \Illuminate\Http\Response
      */
    public function store(Request $request)
-    {
+    { 
+        
+        $this->validate($request, [
+            'content' => 'required|max:10',
+        ]);
+        
         $tasklist = new Tasklist;
         $tasklist->content = $request->content;
         $tasklist->save();
@@ -92,6 +97,10 @@ class TasklistsController extends Controller
      */
     public function update(Request $request, $id)
     {
+         $this->validate($request, [
+            'content' => 'required|max:10',
+        ]);
+        
         $tasklist = Tasklist::find($id);
         $tasklist->content = $request->content;
         $tasklist->save();
